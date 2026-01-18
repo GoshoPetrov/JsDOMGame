c8e0608d56b30d6a0daf2f7beebf40546d7d8d4a
GoshoPetrov <146186451+GoshoPetrov@users.noreply.github.com>
2026-01-18 11:19:48 +0200

    refactor game structure and enhance enemy behavior

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
-function Game(){
+function Game() {
+    let game = document.getElementById('game');
+    
     let player = document.getElementById('player');
     let positionY = 250;
     let positionX = 250;
 
     let enemy = document.getElementById('enemy');
+    let enemies = [];
+    let enemiesCount = 5;
     let enemyX = 100;
     let enemyY = 100;
-    let enemySpeedX = 10;
-    let enemySpeedY = 10;
-    
+    let enemySpeedX = 2;
+    let enemySpeedY = 2;
+
+    for (let i = 0; i < enemiesCount; i++) {
+        let div = document.createElement('div');
+        div.classList.add('enemy');
+        game.appendChild(div);
+
+        enemies.push({
+            el: div,
+            x: Math.random() * (game.clientWidth - 40),
+            y: Math.random() * (game.clientHeight - 40),
+            speedX: enemySpeedX,
+            speedY: enemySpeedY
+        })
+    }
+    //debugger;
     let playerSpeed = 10;
 
     const keysPressed = {};
@@ -19,23 +37,25 @@ function Game(){
         if (keysPressed['a']) positionX -= playerSpeed;
         if (keysPressed['d']) positionX += playerSpeed;
 
-        enemyX += enemySpeedX;
-        enemyY += enemySpeedY;
+        enemies.forEach(enemy => {
+            enemy.x += enemy.speedX;
+            enemy.y += enemy.speedY;
+            //debugger;
+            if (enemy.x <= 0 || enemy.x >= game.clientWidth * 1.9 - enemy.x) {
+                enemy.speedX *= -1;
+            }
 
-        if(enemyX <= 0 || enemyX >= window.innerWidth + 100 - enemyX){
-            enemySpeedX *= -1;
-        }
+            if (enemy.y <= 0 || enemy.y >= game.clientHeight * 1.9 - enemy.y) {
+                enemy.speedY *= -1;
+            }
 
-        if(enemyY <= 0 || enemyY >= window.innerHeight + 2000 - enemyY){
-            enemySpeedY *= -1;
-        }
-
-        enemy.style.top = enemyX + 'px';
-        enemy.style.left = enemyY + 'px';
+            enemy.el.style.top = enemy.y + 'px';
+            enemy.el.style.left = enemy.x + 'px';
+        });
 
         player.style.top = positionY + 'px';
         player.style.left = positionX + 'px';
-    }, 1); 
+    }, 1);
 
     document.addEventListener('keydown', (event) => {
         keysPressed[event.key.toLowerCase()] = true;
