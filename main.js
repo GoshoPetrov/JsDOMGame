55a2e73972483b468fce9934cc10a4f8454c79bb
GoshoPetrov <146186451+GoshoPetrov@users.noreply.github.com>
2026-01-18 10:29:33 +0200

    refactor game initialization and add enemy movement functionality

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-function movement(){
+function Game(){
     let player = document.getElementById('player');
     let positionY = 250;
     let positionX = 250;
+
+    let enemy = document.getElementById('enemy');
+    let enemyX = 100;
+    let enemyY = 100;
+    let enemySpeedX = 10;
+    let enemySpeedY = 10;
     
     let playerSpeed = 10;
 
@@ -12,6 +18,20 @@ function movement(){
         if (keysPressed['s']) positionY += playerSpeed;
         if (keysPressed['a']) positionX -= playerSpeed;
         if (keysPressed['d']) positionX += playerSpeed;
+
+        enemyX += enemySpeedX;
+        enemyY += enemySpeedY;
+
+        if(enemyX <= 0 || enemyX >= window.innerWidth + 100 - enemyX){
+            enemySpeedX *= -1;
+        }
+
+        if(enemyY <= 0 || enemyY >= window.innerHeight + 2000 - enemyY){
+            enemySpeedY *= -1;
+        }
+
+        enemy.style.top = enemyX + 'px';
+        enemy.style.left = enemyY + 'px';
 
         player.style.top = positionY + 'px';
         player.style.left = positionX + 'px';
